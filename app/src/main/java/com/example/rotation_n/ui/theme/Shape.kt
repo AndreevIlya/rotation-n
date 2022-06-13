ea1741640cbe137d67d6ec8c0064fc985fef7591
Ilya Andreev <katauri1024@gmail.com>
2022-06-13 15:30:36 +0300

    RN-2: Add material design styling

--- app/src/main/java/com/example/rotation_n/ui/theme/Shape.kt
+++ app/src/main/java/com/example/rotation_n/ui/theme/Shape.kt
@@ -1,10 +1,11 @@
 package com.example.rotation_n.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Shapes
+import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
+    extraSmall = RoundedCornerShape(2.dp),
     small = RoundedCornerShape(4.dp),
     medium = RoundedCornerShape(4.dp),
     large = RoundedCornerShape(0.dp)
