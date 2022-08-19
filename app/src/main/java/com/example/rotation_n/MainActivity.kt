725181f383b895328a937fbe39860e694832b8e6
Ilya Andreev <katauri1024@gmail.com>
2022-08-19 11:30:58 +0300

    RN-4: Extract top bar as separate dependency

--- app/src/main/java/com/example/rotation_n/MainActivity.kt
+++ app/src/main/java/com/example/rotation_n/MainActivity.kt
@@ -6,24 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.unit.dp
+import com.example.rotation_n.ui.components.TopBar
 import com.example.rotation_n.ui.theme.RotationNTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -43,89 +33,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-
-    @Composable
-    fun TopBar() {
-        SmallTopAppBar(
-            title = {
-                Text(
-                    stringResource(id = R.string.app_name),
-                    color = MaterialTheme.colorScheme.onPrimaryContainer,
-                    style = MaterialTheme.typography.headlineLarge
-                )
-            },
-            colors = TopAppBarDefaults.smallTopAppBarColors(
-                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
-                titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
-            ),
-            navigationIcon = { ImageVector.vectorResource(id = R.drawable.back) },
-            actions = { DimensionMenu() }
-        )
-    }
-
-    @Composable
-    fun DimensionMenu() {
-        val dimension by viewModel.dimension.observeAsState()
-        var expanded by remember { mutableStateOf(false) }
-
-        Box(
-            modifier = Modifier
-                .background(color = MaterialTheme.colorScheme.secondaryContainer)
-                .padding(8.dp)
-                .size(40.dp, 20.dp)
-        ) {
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Text(
-                    (dimension ?: 3).toString(),
-                    color = MaterialTheme.colorScheme.onSecondaryContainer,
-                    style = MaterialTheme.typography.labelLarge
-                )
-                IconButton(
-                    onClick = { expanded = true },
-                    colors = IconButtonDefaults.iconButtonColors(
-                        contentColor = MaterialTheme.colorScheme.onSecondaryContainer
-                    )
-                ) {
-                    Icon(
-                        ImageVector.vectorResource(id = R.drawable.arrow_drop_down),
-                        "Open",
-                        tint = MaterialTheme.colorScheme.onSecondaryContainer
-                    )
-                }
-            }
-            DropdownMenu(
-                expanded = expanded,
-                onDismissRequest = { expanded = false },
-                Modifier.background(MaterialTheme.colorScheme.surface)
-            ) {
-                LazyVerticalGrid(
-                    modifier = Modifier.size(120.dp, 96.dp),
-                    columns = GridCells.Fixed(3)
-                ) {
-                    items((3..7).toList()) { DimensionMenuItem(dim = it) }
-                }
-            }
-        }
-
-    }
-
-    @Composable
-    fun DimensionMenuItem(dim: Int) {
-        val scope = rememberCoroutineScope()
-        DropdownMenuItem(
-            text = {
-                Text(
-                    dim.toString(),
-                    color = MaterialTheme.colorScheme.onSurface,
-                    style = MaterialTheme.typography.labelLarge
-                )
-            }, onClick = {
-                scope.launch {
-                    viewModel.updateDimension(dim)
-                }
-            }
-        )
     }
 }
