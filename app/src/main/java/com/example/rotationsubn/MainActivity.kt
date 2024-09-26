44b3a815a0c503717c4dd6066514e88100d7da6f
Ilya Andreev <katauri1024@gmail.com>
2024-09-26 16:53:25 +0300

    RN-4: Remove default action bar

--- app/src/main/java/com/example/rotationsubn/MainActivity.kt
+++ app/src/main/java/com/example/rotationsubn/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.rotationsubn
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : FragmentActivity(R.layout.main) {
+class MainActivity : AppCompatActivity(R.layout.main) {
 
     private val viewModel by viewModels<MainViewModel>()
 
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        supportActionBar?.hide()
+    }
 }
