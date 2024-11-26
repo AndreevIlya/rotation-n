3a2706fdd0cb821b6a0c319875b3a807521a858c
Ilya Andreev <katauri1024@gmail.com>
2024-11-26 14:07:19 +0300

    RN-2: Replace top bar back default icon button with customary

--- app/src/main/java/com/example/rotationsubn/ui/components/topbar/TopBar.kt
+++ app/src/main/java/com/example/rotationsubn/ui/components/topbar/TopBar.kt
@@ -13,8 +13,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,17 +22,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.onClick
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.DpOffset
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import com.example.rotationsubn.R
+import com.example.rotationsubn.ui.components.IconButton
+import com.example.rotationsubn.ui.components.IconData
 import com.example.rotationsubn.ui.theme.RNTheme
 
 class TopBar(
@@ -65,15 +65,20 @@ class TopBar(
                 ) {
                     if (onBack != null) {
                         IconButton(
-                            modifier = Modifier.size(24.dp),
-                            onClick = onBack,
-                        ) {
-                            Icon(
-                                imageVector = ImageVector.vectorResource(id = R.drawable.ic_back),
+                            modifier = Modifier.clickable { onBack() },
+                            semantics = {
+                                onClick(
+                                    label = "top bar back",
+                                    action = { true }
+                                )
+                            },
+                            size = DpSize(24.dp, 24.dp),
+                            icon = IconData(
+                                res = R.drawable.ic_back,
                                 tint = RNTheme.colors.onPrimaryContainer,
-                                contentDescription = "top bar back",
+                                description = "top bar back"
                             )
-                        }
+                        )
                         Spacer(Modifier.width(RNTheme.gaps.horizontal.lg))
                     }
                     Text(
